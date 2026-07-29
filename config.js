79f88353a655977994f28f3aff492274a92b2fe5
YoavYaacov <yoavyaacov395@gmail.com>
2026-07-29 23:37:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@
 // inside the Supabase Edge Function secrets).
 // =====================================================================
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://uifrgmkbfnndcohwfeof.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://uifrgmkbfnndcohwfeof.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZnJnbWtiZm5uZGNvaHdmZW9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyMjkyNjYsImV4cCI6MjEwMDgwNTI2Nn0.0H46_ejHqbRCkiY9SpJcpry0aLH2i48mXDRS9bSSH5I",
 };
